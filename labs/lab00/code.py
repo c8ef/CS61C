2156d2f271e4cbd35412fe4b4cebb3660e0fbee8
c8ef <c8ef@outlook.com>
2022-09-03 21:20:00 +0800

    finish lab00

--- labs/lab00/code.py
+++ labs/lab00/code.py
@@ -1,18 +1,23 @@
 def get_airspeed_velocity_of(unladen_swallow):
-  if unladen_swallow.type == "african":
-    return # redacted
-  elif unladen_swallow.type == "european":
-    return # redacted
+    if unladen_swallow.type == "african":
+        return  # redacted
+    elif unladen_swallow.type == "european":
+        return  # redacted
+
 
 # pretend there's code here...
 
+
 def fizzbuzz(num):
-  if num == 3: # edit this line
-    print(f"{num}: fizz")
-  if num == 5: # edit this line
-    print(f"{num}: buzz")
+    if num % 15 == 0:  # edit this line
+        print(f"{num}: fizzbuzz")
+    elif num % 5 == 0:  # edit this line
+        print(f"{num}: buzz")
+    elif num % 3 == 0:
+        print(f"{num}: fizz")
+
 
 for i in range(1, 20):
-  fizzbuzz(i)
+    fizzbuzz(i)
 
 # pretend there's code here...
